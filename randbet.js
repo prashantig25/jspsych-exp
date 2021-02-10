167057f19288e6e7aa4d1c4c1a97fd06b3ddc37c
prashantig25 <75947365+prashantig25@users.noreply.github.com>
2021-02-10 11:12:57 +0100

    Delete randbet.js

--- randbet.js
+++
@@ -1,3 +0,0 @@
-function randbet(min, max) { //randbet generates a randomly number between the min and max number. // min and max are the upper and lower bound.
-    return Math.random() * (max - min) + min;
-}
